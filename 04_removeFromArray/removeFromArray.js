8cd0cc9e7a417e21e951e5966906ed8247643c76
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-04 16:34:55 +0100

    the remove from array can do single , multiple ,non present value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...elementsToRemove) {
+ 
+    return array.filter(item => !elementsToRemove.includes(item));
+}
+console.log(removeFromArray([1,2,3,4],3));
+console.log(removeFromArray([1,2,3,4],3,2));
+console.log(removeFromArray([1,2,2,3],2));
+console.log(removeFromArray([1,2,3,4],7,"tacos"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
