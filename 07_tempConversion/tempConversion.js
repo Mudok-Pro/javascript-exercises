c1df8205db03cc53dea6679144ac29c3b996e261
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-05 00:51:13 +0100

    you celcius converter is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let celsius = (tempF - 32)* 5/9 ;
+  let roundedNumber1 = Math.round(celsius *10)/10;
+  return roundedNumber1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let fahrenheit = tempC * 9/5 + 32;
+  let roundedNumber2 = Math.round(fahrenheit *10)/10;
+  return roundedNumber2;
 };
-
+console.log(convertToCelsius(32))
+console.log(convertToCelsius(100))
+console.log(convertToCelsius(-100))
+console.log(convertToFahrenheit(0))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+
+
