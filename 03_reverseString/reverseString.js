d454ec2a2f52173ee8d084b340a0d61c4bed0e88
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-04 13:25:48 +0100

    the function can reverse multiple words

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
-
+function reverseString(str){
+   
+    let result = '';
+    for (let i =0; i < str.length ; i++ ){
+        result = str.charAt(i) + result ; 
+    }
+    return result; 
+}
+console.log(reverseString('hello'));
+console.log(reverseString('hello there'));
 // Do not edit below this line
 module.exports = reverseString;
