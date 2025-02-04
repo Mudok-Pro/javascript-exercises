4ad0ee1762de511bc4d33ed42503e6f2a7088530
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-04 16:37:14 +0100

    the function work very good

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -6,6 +6,7 @@ console.log(removeFromArray([1,2,3,4],3));
 console.log(removeFromArray([1,2,3,4],3,2));
 console.log(removeFromArray([1,2,2,3],2));
 console.log(removeFromArray([1,2,3,4],7,"tacos"));
+console.log(removeFromArray([1,2,3,4],7,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
