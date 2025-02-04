8519a8f89eb0a6c08d6d3db633ede4a189cc6b1a
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-04 23:32:05 +0100

    SumAll is a function to sum just positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt,secondInt) {
+let result = 0; 
+if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)){
+    result = 'ERROR';
+}
+else if (firstInt < 0 || secondInt < 0 ){
+    result = 'ERROR';
+}
+ else if (firstInt > secondInt){
+    for (let i=secondInt; i<= firstInt; i++ ){
+        result = result + i ;
+}}else if (firstInt < secondInt){
+    for (let i=firstInt; i<= secondInt; i++ ){
+        result = result + i ;
+}}
+return result ; 
 };
+console.log(sumAll(1,4))
+console.log(sumAll(1,4000))
+console.log(sumAll(123,1))
+console.log(sumAll(-10,4))
+console.log(sumAll(2.5,4))
+console.log(sumAll(10,"90"))
+console.log(sumAll(10, [90, 1]))
 
 // Do not edit below this line
 module.exports = sumAll;
