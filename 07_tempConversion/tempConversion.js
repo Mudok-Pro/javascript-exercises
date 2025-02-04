58f0d6a465672b230affdc70d78c06bdb3b7d81f
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-05 00:56:42 +0100

    the tempConverter convert temperatur from celcius to fahrenheit and vice-versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -13,6 +13,8 @@ console.log(convertToCelsius(32))
 console.log(convertToCelsius(100))
 console.log(convertToCelsius(-100))
 console.log(convertToFahrenheit(0))
+console.log(convertToFahrenheit(73.3))
+console.log(convertToFahrenheit(-10))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
