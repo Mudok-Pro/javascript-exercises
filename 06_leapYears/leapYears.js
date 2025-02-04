d8854fbd62e2393722c3c46ef6ea79a996743c01
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-05 00:22:04 +0100

    the function leapYears works fine

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
 
+ if ( leapYear % 100 === 0 && leapYear % 400 === 0    )  {
+    
+    return true ;
+ }else if ( leapYear % 4 === 0 && leapYear % 100 !== 0  ){
+    return true ; 
+  } else {
+    return false ;
+}
 };
+console.log(leapYears(1996))
+console.log(leapYears(1997))
+console.log(leapYears(34992))
+console.log(leapYears(1900))
+console.log(leapYears(1600))
+console.log(leapYears(700))
 
 // Do not edit below this line
 module.exports = leapYears;
