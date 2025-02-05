49fff47802f5771fa77b53bd4bf8f27e4992837f
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-05 21:32:52 +0100

    the palindromes checker function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+function reverseString(str){
+   
+    let result = '';
+    for (let i =0; i < str.length ; i++ ){
+        result = str.charAt(i) + result ; 
+    }
+    return result; 
+}
+function cleanString(str){
+const noPunctuation = str.replace(/[.,\/#!$%\^&\*;/\s+/g:{}=\-_`~()]/g, '');
+const lowCaser = noPunctuation.toLowerCase();
+return lowCaser
+}
+const palindromes = function (textToCheck) {
+  const clean = cleanString(textToCheck)
+let reversed = reverseString(clean);
 
+if (reversed === clean){
+    return true;
+}else {
+    return false ;
+}
 };
-
+console.log(palindromes("racecar"))
+console.log(palindromes("racecar!"))
+console.log(palindromes('Racecar!'))
+console.log(palindromes('A car, a man, a maraca.'))
 // Do not edit below this line
 module.exports = palindromes;
