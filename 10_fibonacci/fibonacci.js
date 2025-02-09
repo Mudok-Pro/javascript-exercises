f299046b9fadaa02b5768528152d220b5425aae5
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-09 13:23:58 +0100

    added a fibonacci function, made it accept strings too , and negative numbers return 'OOPS'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+ function fibonacci(num) {
+      if (num <0 ) return "OOPS"
+    if ( num == 1 ) return 1 ;
+    if ( num == 0) return 0 ;
 
-};
+  
+    let a = 1 , b = 1 ; 
+    for (let i = 2; i < num ; i++){
+        let temp = a ; 
+        a = b ; 
+        b = temp + b ;
+    }
+    return b;
+ }
+console.log(fibonacci(4));
+console.log(fibonacci(6));
+console.log(fibonacci(10));
+console.log(fibonacci(15));
+console.log(fibonacci(25));
+console.log(fibonacci(0));
+console.log(fibonacci(-25));
+console.log(fibonacci("0"));
+console.log(fibonacci("1"));
+console.log(fibonacci("2"));
+console.log(fibonacci("8"));
 
 // Do not edit below this line
 module.exports = fibonacci;
